b90ab1f89000eda818859456b00ac045c8090714
Alicia MacEwan-Gutierrez <alicia7749@gmail.com>
2023-07-08 12:23:00 -0700

    adding style

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,7 +3,7 @@ import homeImage from '../images/home-image.png'
 
 const Home = () =>{
     return <div>
-        <img src={homeImage} alt="Nutritious foods" />
+        <img src={homeImage} alt="Nutritious foods" className="homeImage"/>
         <div class="intro">
             <h1>WELCOME!</h1>
             <h3>The world's #1 food and calorie tracking website.
