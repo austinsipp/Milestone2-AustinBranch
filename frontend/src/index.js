ca521b49d21ec81a900b7eb71ef83efc6713c022
austinsipp <austin.sipp@gmail.com>
2023-08-28 14:18:31 -0700

    use scss for styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import './css/style.css';
 import App from './App';
 import {FoodContextProvider} from './context/foodContext'
 import 'bootstrap/dist/css/bootstrap.min.css';
